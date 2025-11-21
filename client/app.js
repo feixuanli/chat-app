7d8a19b8599bde40722b9b314eb1939f301c732a
chaochao <feixuann.li@gmail.com>
2025-11-20 19:48:21 -0800

    wrap communication

--- client/app.js
+++ client/app.js
@@ -11,12 +11,15 @@ const sendMessage = (e) => {
   const input = document.querySelector("input");
   if (input.value) {
     socket.send(input.value);
+    input.value = '';
   }
 };
 
 form.addEventListener("submit", sendMessage);
 
 socket.addEventListener("message", ({ data }) => {
-  console.log("hi");
-  console.log(data);
+  const ulElement = document.querySelector('ul');
+  const li = document.createElement('li');
+  li.textContent = data;
+  ulElement.appendChild(li);
 });
