bd71f29ecbac8b166e21bc09b72d41d2c59be776
chaochao <feixuann.li@gmail.com>
2025-11-20 19:54:22 -0800

    configure http server

--- server/app.js
+++ server/app.js
@@ -3,12 +3,23 @@
 const ws = require('ws');
 const http = require('http');
 
-const wsServer = new ws.WebSocketServer({
-    port: 3001
-});
+// have http server and ws server to listern to different ports 
+// const wsServer = new ws.WebSocketServer({
+//     port: 3001
+// });
+
+// const httpServer = http.createServer();
+// httpServer.listen(3002)
+
+// have http server and ws server to listern to same ports 
 
 const httpServer = http.createServer();
-httpServer.listen(3002)
+httpServer.listen(3001);
+
+const wsServer = new ws.WebSocketServer({
+    server: httpServer
+});
+
 
 // make the server to listen to a pariticular event from client 
 
